eefc7d71d202b7c2b5f8d1495a8f8f9b60a3d530
simonini <lcs.simonini@gmail.com>
2017-03-23 18:22:45 -0300

    inicial

---
+++ src/main/java/br/com/integrador/ScrumboardGamingApplication.java
@@ -0,0 +1,12 @@
+package br.com.integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumboardGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumboardGamingApplication.class, args);
+	}
+}
